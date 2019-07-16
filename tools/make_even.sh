b300edb9a5f6d0924b6c1c6d6c896c06e923fe07
math-alpha <mathalpha26@gmail.com>
2019-07-16 23:32:40 +0100

    added more words

---
+++ tools/make_even.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo  "Making .bidix and . files lexc even again ..."
+'
+        Algorithm
+    - Read file (input file)
+    - Find pattern
+    - Copy all hits to output file
+    - Modify entries in output file to match .lexc file standard
+'
